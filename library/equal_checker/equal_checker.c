8043622c7f6a6047f0b54fed17b610e26bc7d944
Peter Tönz <peter@rideyourstyle.ch>
2026-02-18 16:20:06 +0100

    test west-init 15

--- library/equal_checker/equal_checker.c
+++
@@ -1,7 +0,0 @@
-#include "equal_checker.h"
-
-
-bool isEqual(const uint32_t a, const uint32_t b)
-{
-    return a == b;
-}
--- library/equal_checker/equal_checker.h
+++
@@ -1,9 +0,0 @@
-#ifndef EQUAL_CHECKER_H
-#define EQUAL_CHECKER_H
-#include <stdbool.h>
-#include <stdint.h>
-
-bool isEqual(uint32_t a, uint32_t b);
-
-
-#endif //EQUAL_CHECKER_H
